22f8ea47c8a31c0d9c3eacf9201bdcf254eb3e95
Paolo Centomani <paolo.centomani@gmail.com>
2024-12-28 22:57:52 +0100

    Add “URL IP Address”

---
+++ url-ip-address.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title URL IP Address
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌐
+# @raycast.argument1 { "type": "text", "placeholder": "URL", "optional": false}
+# @raycast.argument2 { "type": "dropdown", "data": [{"title": "IPv4", "value": "ipv4"}, {"title": "IPv6", "value": "ipv6"}], "placeholder": "Mode (Default: IPv4)", "optional": true}
+
+# Documentation:
+# @raycast.description Copy the IP address of the given URL.
+# @raycast.author Paolo Centomani
+# @raycast.authorURL https://noveria.it
+
+domain=${1#*//}
+domain=${domain%%/*}
+
+if [[ "$2" == "ipv6" ]]
+then
+    mode="-6"
+else
+    mode="-4"
+fi
+
+ip=$(dig $mode +short +time=1 $domain | grep -v "\.$" | awk "{ print ; exit }")
+echo $ip | pbcopy
+echo "Copied $ip"
