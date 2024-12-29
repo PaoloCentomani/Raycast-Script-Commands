23c3c66ed45ca3dda32e9ab610fd3043ba153acc
Paolo Centomani <paolo.centomani@gmail.com>
2024-12-29 15:35:21 +0100

    Add “Count Characters”

---
+++ count-characters.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Count Characters
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🔢
+# @raycast.argument1 { "type": "text", "placeholder": "Text (Default: clipboard)", "optional": true }
+
+# Documentation:
+# @raycast.description Counts the characters of either the clipboard or the passed argument.
+# @raycast.author Paolo Centomani
+# @raycast.authorURL https://noveria.it
+
+arg=$1
+
+if [[ -z "$arg" ]]
+then
+    str=$(pbpaste)
+    echo "${#str} characters"
+else
+    echo "${#arg} characters"
+fi
